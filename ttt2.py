d4fa8c68fe14b6e57145f2f40e069431334a471d
MikkeL0730 <57572232+MikkeL0730@users.noreply.github.com>
2023-10-16 18:10:56 +0400

    Update ttt2.py

--- ttt2.py
+++ ttt2.py
@@ -2,34 +2,27 @@ import tkinter as tk
 from tkinter import messagebox
 import random
 
-# Создание окна игры
 window = tk.Tk()
 window.title("Крестики-нолики")
 
-# Определение размера окна и ячеек
 cell_size = 100
 canvas_size = cell_size * 3
 
 canvas = tk.Canvas(window, width=canvas_size, height=canvas_size)
 canvas.pack()
 
-# Инициализация игрового поля
 board = [[" " for _ in range(3)] for _ in range(3)]
 
-# Определение текущего игрока
 current_player = "X"
 
 
-# Функция для отрисовки игрового поля
 def draw_board():
     canvas.delete("all")
 
-    # Рисование сетки
     for i in range(1, 3):
         canvas.create_line(0, i * cell_size, canvas_size, i * cell_size)
         canvas.create_line(i * cell_size, 0, i * cell_size, canvas_size)
 
-    # Рисование крестиков и ноликов
     for i in range(3):
         for j in range(3):
             if board[i][j] == "X":
@@ -44,19 +37,15 @@ def draw_board():
                 canvas.create_oval(x - radius, y - radius, x + radius, y + radius)
 
 
-# Функция для обработки хода игрока
 def handle_click(event):
     if current_player == "X":
-        # Определение координат ячейки, на которую кликнул игрок
         row = event.y // cell_size
         col = event.x // cell_size
 
-        # Проверка, что выбранная ячейка пуста
         if board[row][col] == " ":
             board[row][col] = current_player
             draw_board()
 
-            # Проверка на победу
             if check_win(current_player):
                 messagebox.showinfo("Игра окончена!", f"Игрок {current_player} победил!")
                 reset_game()
@@ -68,7 +57,6 @@ def handle_click(event):
                 make_ai_move()
 
 
-# Функция для переключения игрока
 def switch_player():
     global current_player
     if current_player == "X":
@@ -77,7 +65,6 @@ def switch_player():
         current_player = "X"
 
 
-# Функция для проверки победы
 def check_win(player):
     for i in range(3):
         if board[i][0] == board[i][1] == board[i][2] == player:
@@ -91,7 +78,6 @@ def check_win(player):
     return False
 
 
-# Функция для проверки ничьей
 def check_draw():
     for i in range(3):
         for j in range(3):
@@ -100,7 +86,6 @@ def check_draw():
     return True
 
 
-# Функция для сброса игры
 def reset_game():
     global board, current_player
     board = [[" " for _ in range(3)] for _ in range(3)]
@@ -108,9 +93,7 @@ def reset_game():
     draw_board()
 
 
-# Функция для хода искусственного интеллекта
 def make_ai_move():
-    # Создание списка доступных ходов
     available_moves = []
     lasti = -1
     lastj = -1
@@ -135,14 +118,9 @@ def make_ai_move():
                 i = lasti
                 j = lastj
 
-    # Выбор случайного доступного хода
-    #row, col = random.choice(available_moves)
-
-    # Выполнение хода и отрисовка игрового поля
     board[i][j] = current_player #row col
     draw_board()
 
-    # Проверка на победу
     if check_win(current_player):
         messagebox.showinfo("Игра окончена!", f"Игрок {current_player} победил!")
         reset_game()
